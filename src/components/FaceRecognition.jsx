aa8f88e0a5076ed0f3d66f1dc35454c1ae7aaae0
Varun S P <spvarun47@gmail.com>
2025-02-03 09:36:19 +0530

    Added more models

--- src/components/FaceRecognition.jsx
+++ src/components/FaceRecognition.jsx
@@ -15,9 +15,9 @@ const FaceRecognition = () => {
   useEffect(() => {
     const loadModels = async () => {
       try {
-        // await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
-        // await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
-        // await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+        await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+        await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+        await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
         await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
         console.log("Models loaded successfully");
       } catch (error) {
